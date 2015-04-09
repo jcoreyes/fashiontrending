849b38354f5e29a0bc6e4e36de4a7d044ee9986e
Grace Park <epark@caltech.edu>
2015-04-09 00:34:26 -0700

    added a tag filter file, text file for seed tag words, and a list of shadow words we want to filter out

--- textcrawler/crawler.py
+++ textcrawler/crawler.py
@@ -1,12 +1,20 @@
+import time
 from crawler_loop import * 
 
 def main():
+	"""
+	Crawl the instagram posts and find popular tags related to fashion
+	"""
+	# holds all the tags and occurences
 	tag_dict = {}
 	textfile = file('tag_crawler_output.txt','wt')
 	
-	# loop this part
-	media_info = get_info()
-	get_tags(media_info, tag_dict)
+	# retrieve data from instagram every 10 seconds
+	for i in xrange(10):
+		# every 10 seconds, gather information regarding fashion posts
+		time.sleep(10)
+		media_info = get_info()
+		get_tags(media_info, tag_dict)
 	for key in tag_dict.keys():
 		strr = str(key) + " , " + str(tag_dict[key]) + "\n"
 		textfile.write(strr)
---
+++ textcrawler/shadow_words.py
@@ -0,0 +1,30 @@
+
+# list of words that might overshadow meaningful results
+# will add more 
+
+shadow = [
+"#beauty",
+"#beautiful",
+"#happy",
+"#joy",
+"#smile",
+"#happy",
+"#summer",
+"#cute",
+
+"#stylish",
+"#trendy",
+"#favorite",
+"#look",
+
+"#nofilter",
+"#selfie",
+
+"#ifollowback",
+"#instafollow",
+"#followme",
+"#teamfollowback",
+"#follow",
+"#tagsforlikes",
+"#instalikes"
+]
